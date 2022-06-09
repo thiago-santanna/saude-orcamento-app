1ecbbb146b6b46f8f30fce2b7d7bebfbd4c15036
thiago-santanna <uzmaki_thiago@outlook.com>
2022-06-09 08:32:51 -0300

    selecinoe seu plano

--- pages/_app.js
+++ pages/_app.js
@@ -5,7 +5,7 @@ import '../styles/globals.css'
 function MyApp({ Component, pageProps }) {
   return (
     <>
-      <header className="border-b-4 border-indigo-600 p-4 bg-white text-white h-28 flex items-center">
+      <header className="border-b-4 mt-2 border-sky-600 p-4 bg-white text-white h-28 flex items-center">
         <Image src="/logo.png" alt='Logo Marca' width='332' height='132' />
         <Header />
       </header>
--- pages/index.js
+++ pages/index.js
@@ -5,14 +5,14 @@ export default function Home() {
 
         <h1 className='font-bold text-2xl p-8'>Monte seu plano</h1>
         <p className='p-2 text-center mb-4'>Precisamos de algumas informações para sugerirmos o melhor plano.</p>
-        <div className='bg-gray-300 p-4 w-full text-center'>
-          <span className='text-xl'>Possui plano?</span>
+        <div className='bg-sky-600 text-white p-4 w-full text-center'>
+          <span className='text-xl font-bold'>Possui plano?</span>
           <div className='flex items-center justify-between m-4'>
             <div className='flex items-center'>
               <input className='h-8 w-8 mr-4' name='sim' type="radio" />
               <label
                 htmlFor='sim'
-                className='block text-lg font-semibold text-gray-600'
+                className='block text-lg font-semibold text-white'
               >
                 SIM
               </label>
@@ -22,13 +22,26 @@ export default function Home() {
               <input className='h-8 w-8 mr-4' name='nao' type="radio" />
               <label
                 htmlFor='sim'
-                className='block text-lg font-semibold text-gray-600'
+                className='block text-lg font-semibold text-white'
               >
                 NÃO
               </label>
             </div>
           </div>
         </div>
+
+        <div className="w-full">
+          <h1 className='font-bold text-2xl p-8'>Qual seu plano?</h1>
+          <div className='bg-sky-600 p-4 text-center'>
+            <select className="w-full p-2">
+              <option value="SELECIONE" key="SELECIONE">SELECIONE</option>
+              <option value="AMIL" key="AMIL">AMIL</option>
+              <option value="BRADESCO" key="BRADESCO">BRADESCO</option>
+              <option value="UNIMED " key="UNIMED">UNIMED</option>
+            </select>
+          </div>
+        </div>
+
       </div>
     </>
   )
