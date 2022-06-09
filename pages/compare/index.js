350fbecff18502058816894863738fb4782295f0
thiago-santanna <uzmaki_thiago@outlook.com>
2022-06-09 14:49:56 -0300

    nova pagina de comparacao de planos

---
+++ pages/compare/index.js
@@ -0,0 +1,10 @@
+
+export default function Compare() {
+  return (
+    <>
+      <div className='h-screen  flex flex-col items-center bg-white'>
+        <h1 className='font-bold text-2xl p-8'>Plano sugeridos</h1>
+      </div>
+    </>
+  )
+}
