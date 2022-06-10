0f8d25591e24623fed9e4d1699a7715ffc1d5b2c
thiago-santanna <uzmaki_thiago@outlook.com>
2022-06-10 08:50:55 -0300

    butao no compnente de plano

--- components/plano-compare/index.js
+++ components/plano-compare/index.js
@@ -1,3 +1,4 @@
+import Link from "next/link";
 
 export default function PlanoCompare() {
   return (
@@ -7,21 +8,38 @@ export default function PlanoCompare() {
           <span className="font-bold">CD1020</span>
           <span>AMIL</span>
         </div>
+
         <div className="flex flex-col gap-2">
           <span className="font-bold">Classificação</span>
           <span>X X X X X</span>
         </div>
+
         <div className="flex flex-col gap-2">
           <span className="font-bold">Abrangência</span>
           <span>Nacional</span>
         </div>
+
         <div className="flex flex-col gap-2">
           <span className="font-bold">Mensalidade</span>
           <span>R$ 585,00</span>
         </div>
+
         <div className="flex flex-col gap-3">
           <span className="font-bold">Acomodação</span>
           <span>Apartamento</span>
+        </div>
+
+        <div className='mt-6 text-base'>
+          <button
+            type='button'
+            // onClick={handleInsertUser}
+            className='w-full px-4 py-2 tracking-wide text-white text-center transition-colors duration-200 transform bg-sky-600 rounded-md hover:bg-sky-400 focus:outline-none focus:bg-sky-400 focus:ring focus:ring-sky-300 focus:ring-opacity-50'
+          >
+            {/* <Edit size={24} color='white' className='mr-3 inline' /> */}
+            <Link href='#'>
+              <a className='text-lg pl-1 focus:outline-none focus:underline hover:underline'>Selecionar</a>
+            </Link>
+          </button>
         </div>
       </div>
     </>
