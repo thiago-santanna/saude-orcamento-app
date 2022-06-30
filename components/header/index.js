ae9ce5f0fb75edb0488e8cb89d83b6e07ab9b245
thiago-santanna <uzmaki_thiago@outlook.com>
2022-06-30 09:45:15 -0300

    versao pronta pra api

--- components/header/index.js
+++ components/header/index.js
@@ -1,12 +1,12 @@
-import Head from "next/head";
+import Head from 'next/head'
 
 export default function Header() {
   return (
     <>
       <Head>
-        <title>Orcamento Saúde</title>
+        <title>Orcamento planos de saúde</title>
         <meta name='viewport' content='initial-scale=1.0, width=device-width' />
       </Head>
     </>
-  );
+  )
 }
--- components/porte-entidade/index.js
+++ components/porte-entidade/index.js
@@ -1,23 +1,18 @@
-
 export default function Port() {
   return (
     <>
       <div className='flex gap-2 border-l-4 border-sky-600 p-2 text-center justify-center'>
-        <div className="p-1">
-          <button className="w-1/4 text-2xl">-</button>
-          <input className="w-1/2 border-2 border-sky-600" type='number' />
-          <button className="w-1/4 text-2xl">+</button>
+        <div className='p-1'>
+          <button className='w-1/4 text-2xl'>-</button>
+          <input className='w-1/2 border-2 border-sky-600' type='number' />
+          <button className='w-1/4 text-2xl'>+</button>
         </div>
 
-        <div className="p-1">
-          <select className="p-2 text-gray-800">
-            <option value="SELECIONE" key="SELECIONE">SELECIONE</option>
-            <option value="SELECIONE" key="SELECIONE">SELECIONE</option>
-            <option value="SELECIONE" key="SELECIONE">SELECIONE</option>
-            <option value="SELECIONE" key="SELECIONE">SELECIONE</option>
-            <option value="SELECIONE" key="SELECIONE">SELECIONE</option>
-            <option value="SELECIONE" key="SELECIONE">SELECIONE</option>
-            <option value="SELECIONE" key="SELECIONE">SELECIONE</option>
+        <div className='p-1'>
+          <select className='p-2 text-gray-800'>
+            <option value='SELECIONE' key='SELECIONE'>
+              SELECIONE
+            </option>
           </select>
         </div>
       </div>
